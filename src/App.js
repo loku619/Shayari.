d42c03a55fa14d4e56f2934818b5510fe906b33f
loku619 <lokesh619srivastava@gmail.com>
2025-02-15 22:18:33 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Header from './components/Header';
+import PoetryGrid from './components/PoetryGrid';
+import Footer from './components/Footer';
+import { CssBaseline, Container } from '@mui/material';
+import backgroundTheme from './assets/images/backgroundTheme.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CssBaseline />
+    <Header />
+  
+    {/* Full-Page Gradient Background */}
+    <div
+      style={{
+        background: "linear-gradient(135deg, #b5b4b4, #b5b4b4)", // 3D Gradient Effect
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: -1,
+      }}
+    />
+  
+    {/* Content */}
+    <Container
+      maxWidth="lg"
+      style={{
+        marginTop: "20px",
+        position: "relative",
+        zIndex: 1,
+      }}
+    >
+      <PoetryGrid />
+    </Container>
+  
+    <Footer />
+  </>
+  
+      
   );
 }
 
